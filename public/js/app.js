d085e90ec7d8209ae221400958b0de6fe5f1952e
ralph <ralph.metel@gmx.de>
2021-06-26 23:22:36 +0200

    updated files

--- public/js/app.js
+++ public/js/app.js
@@ -16034,8 +16034,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /home/alex/Dokumente/coding/vue/wineApp/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /home/alex/Dokumente/coding/vue/wineApp/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /home/ralph/Projects/wineApp/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /home/ralph/Projects/wineApp/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
